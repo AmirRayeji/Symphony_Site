655c63b7033892e812ae820df9bd0bb0e4517cf5
Amir <amirmohamadrayeji@gmail.com>
2022-05-19 16:30:01 +0430

    add static files to project

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 MEDIA_URL='/madia/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- music/views.py
+++ music/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
 
-# Create your views here.
+from music.models import Music
+
+
+def MusicListVeiw(request):
+    music_list=Music.objects.all()
+
+    context={
+        'musiclist':music_list
+    }
+
+    return render(request, 'music/music_list.html', context)
